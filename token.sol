9f5fad8dd5dc53423c2a97d2ad738d474bdd4e19
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2023-10-04 16:15:26 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,7 +1,10 @@
 // // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+//Declaring the solidity version.
 
 interface IERC20 {
+
+ // ** Calling the ERC-20 Interface to implement its functions **.
 
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
@@ -16,6 +19,7 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+// ** Actual token contract **.
 
 contract SampleToken is IERC20 {
     using SafeMath for uint256;
@@ -23,24 +27,34 @@ contract SampleToken is IERC20 {
     string public constant name = "SampleToken";
     string public constant symbol = "SMT";
     uint8 public constant decimals = 18;
+    
+    // ** Creating two mapping functions that will grant users the ability to spend these tokens **.
 
     mapping(address => uint256) balances;
     mapping(address => mapping (address => uint256)) allowed;
 
     uint256 totalSupply_;
 
+    // ** Initializing the constructor and total supply value and balances for our token. Declaring the total supply of the token to be equal to your wallet’s balance for this token **.
+
     constructor(uint256 total) {
          totalSupply_ = total;
          balances[msg.sender] = totalSupply_;
      }
+    
+    // **Function totalSupply which will govern the total supply of our token **.
 
     function totalSupply() public override view returns (uint256) {
         return totalSupply_;
     }
+    
+    // **Function balanceOf which will check the balance of a wallet address **.
 
     function balanceOf(address tokenOwner) public override view returns (uint256) {
         return balances[tokenOwner];
     }
+    
+    // ** Function transfer which will execute the transfer of tokens from the total supply to users **.
 
     function transfer(address receiver, uint256 numTokens) public override returns (bool) {
         require(numTokens <= balances[msg.sender]);
@@ -50,15 +64,21 @@ contract SampleToken is IERC20 {
         return true;
     }
 
+    // ** Function approve which will check if the total supply has the amount of token which needs to be allocated to a user **.
+
     function approve(address delegate, uint256 numTokens) public override returns (bool) {
         allowed[msg.sender][delegate] = numTokens;
         emit Approval(msg.sender, delegate, numTokens);
         return true;
     }
 
+    // ** Function allowance which will check if a user has enough balance to perform the transfer to another user **.
+
     function allowance(address owner, address delegate) public override view returns (uint) {
         return allowed[owner][delegate];
     }
+
+    // ** Function transferFrom which will facilitate the transfer of token between users **.
 
     function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
         require(numTokens <= balances[owner]);
@@ -72,6 +92,7 @@ contract SampleToken is IERC20 {
     }
 }
 
+// ** Calling the Safe Math interface to use math functions in our contract **.
 library SafeMath {
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
       assert(b <= a);
